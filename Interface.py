e131692a901400f4f2f8a5552b969a58b6b894cb
Harsh Panwar <E22CSEU0752@bennett.edu.in>
2023-09-09 20:16:59 +0530

    Added Natural language processing support
    Added natural language processing support(NLP.py)
    created dialog box UI
    added new dependancies
    made the chat box dynamic

--- Interface.py
+++ Interface.py
@@ -2,167 +2,137 @@ import sys
 
 from PyQt6 import QtWidgets
 from PyQt6.QtCore import Qt, QTimer
-from PyQt6.QtWidgets import QLabel, QSizePolicy, QSpacerItem, QVBoxLayout, QFrame, QTextEdit
+from PyQt6.QtWidgets import QLabel, QSizePolicy, QSpacerItem, QVBoxLayout, QFrame, QTextEdit, QDialog
 
 from main_interface import Ui_MainWindow
+from dialog import Ui_Dialog
+import NLP as nlp
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
-        self.mic_button.clicked.connect(self.addchat)
-        #self.ui.setStyleSheet(self.ui.styleSheet())
-        #self.cnt = 1
-        #self.scrollArea.setStyleSheet()  # Change to your desired background color
-        # self.textEdit.setStyleSheet('''
-        #     QScrollBar:vertical {
-        #         background: transparent;
-        #         width: 10px; /* Adjust the width of the scrollbar */
-        #     }
-        #
-        #     QScrollBar::handle:vertical {
-        #         background: red; /* Scrollbar handle color */
-        #         border-radius: 5px; /* Rounded corners */
-        #     }
-        #
-        #     QScrollBar::handle:vertical:hover {
-        #         background: #505050; /* Hovered color */
-        #     }
-        #
-        #     QScrollBar::handle:vertical:pressed {
-        #         background: #303030; /* Pressed color */
-        #     }
-        #
-        #     QScrollBar::sub-page:vertical {
-        #         background: transparent;
-        #     }
-        #
-        #     QScrollBar::add-page:vertical {
-        #         background: transparent;
-        #     }
-        #
-        #     QScrollBar::add-line:vertical, QScrollBar::sub-line:vertical {
-        #         height: 0;
-        #     }
-        #
-        #     QScrollBar::up-arrow:vertical, QScrollBar::down-arrow:vertical {
-        #         width: 0;
-        #     }
-        #
-        #     QScrollBar::sub-line:vertical {
-        #         border: none;
-        #         background: transparent;
-        #     }
-        #
-        #     QScrollBar::sub-line:vertical:pressed {
-        #         background: transparent;
-        #     }
-        #
-        #     QScrollBar::add-line:vertical {
-        #         border: none;
-        #         background: transparent;
-        #     }
-        #
-        #     QScrollBar::add-line:vertical:pressed {
-        #         background: transparent;
-        #     }
-        # ''')
-        #
-        # # Set the CSS styles for QScrollArea scrollbar
-        # self.scrollArea.setStyleSheet('''
-        #     QScrollBar:vertical {
-        #         background: transparent;
-        #         width: 10px; /* Adjust the width of the scrollbar */
-        #     }
-        #
-        #     QScrollBar::handle:vertical {
-        #         background: green; /* Scrollbar handle color */
-        #         border-radius: 5px; /* Rounded corners */
-        #     }
-        #
-        #     QScrollBar::handle:vertical:hover {
-        #         background: #505050; /* Hovered color */
-        #     }
-        #
-        #     QScrollBar::handle:vertical:pressed {
-        #         background: #303030; /* Pressed color */
-        #     }
-        #
-        #     QScrollBar::sub-page:vertical {
-        #         background: transparent;
-        #     }
-        #
-        #     QScrollBar::add-page:vertical {
-        #         background: transparent;
-        #     }
-        #
-        #     QScrollBar::add-line:vertical, QScrollBar::sub-line:vertical {
-        #         height: 0;
-        #     }
-        #
-        #     QScrollBar::up-arrow:vertical, QScrollBar::down-arrow:vertical {
-        #         width: 0;
-        #     }
-        #
-        #     QScrollBar::sub-line:vertical {
-        #         border: none;
-        #         background: transparent;
-        #     }
-        #
-        #     QScrollBar::sub-line:vertical:pressed {
-        #         background: transparent;
-        #     }
-        #
-        #     QScrollBar::add-line:vertical {
-        #         border: none;
-        #         background: transparent;
-        #     }
-        #
-        #     QScrollBar::add-line:vertical:pressed {
-        #         background: transparent;
-        #     }
-        # ''')
+        self.mic_button.clicked.connect(self.opndlg)
 
-
-
-
-    def addchat(self,flag=0,text=None):
-        #prin = "hello" + str(self.cnt)
+    def addchat(self, obj=None, flag=0, text=None):
+        if(text=="Sorry, I could not understand your speech." or text=="Sorry, an error occurred"):
+            flag=2
+        if(flag==0):
+            user="User:\n"
+        else:
+            user="KCLERY:\n"
         text_edit = QTextEdit()
-        text_edit.setPlainText("Hello\nhh\nhhhn\nhhh\n6y6y6y\ny6y6uy6u\ny6y6u6u7u\ny66uy6u7u\ny6uy6u7u7\nu7u7u")
-        #self.cnt += 1
+        text_edit.setPlainText(user+text)
         text_edit.setReadOnly(True)
         text_edit.setMinimumHeight(100)
         text_edit.setMaximumHeight(100)
         text_edit.setMaximumWidth(350)
         text_edit.setContentsMargins(1, 1, 1, 1)
         text_edit.setFrameShape(QFrame.Shape.Box)
-        text_edit.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
+        if(flag==0):
+            text_edit.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+            color="lightgreen"
+        else:
+            text_edit.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
+            color="magenta"
         text_edit.setStyleSheet("QTextEdit {"
-                                "background-color: lightblue;"
+                                "background-color:"+color+";"
                                 "border-radius: 10px;"
 
                                 "}"
-                                "QScrollBar:vertical {"
-                                "background: #f5f5f5;"
-                                "width: 5px;"
-                                "}"
-                                "QScrollBar::handle:vertical {"
-                                "background: #999999;"
-                                "min-height: 20px;"
-                                "border-radius: 10px;"
-                                "}"
-                                "QScrollBar::add-line:vertical, QScrollBar::sub-line:vertical {"
-                                "background: none;"
-                                "}")
+                                """QScrollBar:vertical {
+    background: transparent;
+    width: 5px; /* Adjust the width of the scrollbar */
+}
+
+QScrollBar::handle:vertical {
+    background: blue; /* Scrollbar handle color */
+    border-radius: 2px;/* Rounded corners */
+}
+
+
+
+
+
+QScrollBar::handle:vertical:hover {
+    background: #505050; /* Hovered color */
+}
+
+
+QScrollBar::handle:vertical:pressed {
+    background: #303030; /* Pressed color */
+}
+
+
+QScrollBar::sub-page:vertical {
+    background: transparent;
+}
+
+QScrollBar::add-page:vertical {
+    background: transparent;
+}
+
+
+QScrollBar::add-line:vertical, QScrollBar::sub-line:vertical {
+    height: 0;
+}
+
+QScrollBar::up-arrow:vertical, QScrollBar::down-arrow:vertical {
+    width: 0;
+}
+
+
+QScrollBar::sub-line:vertical {
+    border: none;
+    background: transparent;
+}
+
+
+QScrollBar::sub-line:vertical:pressed {
+    background: transparent;
+}
+
+
+QScrollBar::add-line:vertical {
+    border: none;
+    background: transparent;
+}
+
+
+QScrollBar::add-line:vertical:pressed {
+    background: transparent;
+}
+""")
 
         self.verticalLayout_2.addWidget(text_edit)
         QTimer.singleShot(100, self.scrblw)
         self.verticalLayout_2.setSpacing(18)
+        if obj != None and (flag==0 or flag==2):
+            obj.cls()
 
     def scrblw(self):
         self.scrollArea.verticalScrollBar().setValue(self.scrollArea.verticalScrollBar().maximum())
+
+    def opndlg(self):
+        opn = dlg(self)
+        QTimer.singleShot(50, lambda: self.lp(opn))
+        opn.exec()
+
+    def lp(self, obj):
+        arr = nlp.lang_process()
+        if arr[0]:
+            self.addchat(obj, 0, arr[1])
+
+
+class dlg(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+    def cls(self):
+        self.close()
 
 
 app = QtWidgets.QApplication(sys.argv)
---
+++ NLP.py
@@ -0,0 +1,32 @@
+import speech_recognition as sr
+
+
+def lang_process():
+    # Create a recognizer instance
+    recognizer = sr.Recognizer()
+
+    # Capture audio from the microphone
+    with sr.Microphone() as source:
+        print("Say something...")
+        recognizer.adjust_for_ambient_noise(source, duration=0.2)
+        audio = recognizer.listen(source)
+
+    # Recognize the speech using Google Web Speech API
+
+
+    try:
+        text = recognizer.recognize_google(audio)
+        text = text.lower()
+        print(f"You said: {text}")
+    except sr.UnknownValueError:
+        text = "Sorry, I could not understand your speech."
+        print("Sorry, I could not understand your speech.")
+    except sr.RequestError as e:
+        text = "Sorry, an error occurred"
+        print(f"Sorry, an error occurred: {e}")
+
+    arr = [True, text]
+    return arr
+
+
+#lang_process()
